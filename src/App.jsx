4f702ef0ec3de303ad72ca7a06822cd321b41636
Parthib Deb <debparthib0040@gmail.com>
2024-09-08 13:57:54 +0530

    Finish project

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
 import MacContainer from './MacContainer'
-// import './index.css'
 import './style.css'
 
 function App() {
   return (
-  <div className='w-full h-screen'>
-    <div className='text-white flex flex-col items-center absolute top-24 left-1/2 -translate-x-1/2 font-sans'>
+  <div className='w-full h-screen text-white'>
+
+    <div className="navbar flex gap-10 pt-10 pb-3 absolute top-0 left-1/2 -translate-x-1/2 line ">
+      {[
+        'Store', 'Mac', 'iPad', 'iPhone', 'Airpods', 'Watch', 'Accessories', 'Entertainment','Support'
+      ].map((e)=>(
+        <a href="" className='font-[500] text-sm' key={e}>{e}</a>
+      ))}
+    </div>
+
+    <div className='flex flex-col items-center absolute top-24 left-1/2 -translate-x-1/2 font-sans'>
       <h3 className='text-5xl tracking-tighter font-[600] glow-text'>mackbook pro.</h3>
       <h5>Oh no pro!</h5>
+      <p className='text-center w-3/4 text-sm'>Buy MacBook Pro starting at ₹158900.00 with education savings. And now up to 30 Sep, get AirPods.◊</p>
     </div>
+
     <Canvas camera={{fov:12, position:[0,-12,220]}}>
       {/* <OrbitControls/> */}
       <Environment
@@ -23,6 +33,7 @@ function App() {
         <MacContainer/>
       </ScrollControls>
     </Canvas>
+
   </div>
   )
 }
